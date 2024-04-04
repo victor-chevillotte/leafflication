f21e17f02e27ee37979ef612e96e592c28d6ddd6
victor <victor.chevillotte@gmail.com>
2024-04-04 09:50:48 +0200

    feat: fixed norm

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 # import argparse
 import numpy as np
+
 # from plantcv import plantcv as pcv
 # from plantcv.parallel import WorkflowInputs
 import tensorflow as tf
@@ -9,31 +10,35 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
 
+
 def main():
     try:
         dir = "./data/images"
-        model = load_model('first_saved_model.keras')
+        model = load_model("first_saved_model.keras")
         seed = 123
         batch_size = 32
         img_height = 256
         img_width = 256
         validation_data = tf.keras.utils.image_dataset_from_directory(
             dir,
-            validation_split = 0.2,
-            subset = "validation",
-            seed = seed,
-            image_size = (img_height, img_width),
-            batch_size = batch_size)
+            validation_split=0.2,
+            subset="validation",
+            seed=seed,
+            image_size=(img_height, img_width),
+            batch_size=batch_size,
+        )
 
-        normalization_layer = layers.Rescaling(1./255) # 0 - 255 to 0 - 1
-        normalized_validation_data = validation_data.map(lambda x, y: (normalization_layer(x), y))
+        normalization_layer = layers.Rescaling(1.0 / 255)  # 0 - 255 to 0 - 1
+        normalized_validation_data = validation_data.map(
+            lambda x, y: (normalization_layer(x), y)
+        )
 
         class_names = validation_data.class_names
         print(f"Class names : {class_names}")
         predictions = model.predict(normalized_validation_data)
         print(f"Predictions : {predictions}")
 
-                # Initialize lists to hold actual labels and predicted labels
+        # Initialize lists to hold actual labels and predicted labels
         actual_labels = []
         predicted_labels = []
         index = 0
@@ -42,20 +47,19 @@ def main():
             for i in range(len(labels)):
                 # get the actual label image by image
                 predicted_index = np.argmax(predictions[index])
-                predicted_class_name = validation_data.class_names[predicted_index]
+                predicted_class_name = validation_data.class_names[
+                    predicted_index
+                ]
                 real_class_name = validation_data.class_names[labels[i]]
                 print("=============================")
                 print(f"Prediction : {predicted_class_name}")
                 print(f"Real class : {real_class_name}")
                 index += 1
-      
-
 
         # Evaluate the model
         score = model.evaluate(normalized_validation_data)
         print(f"Score : {score}")
 
-        
     except Exception as e:
         print(f"An error has occured : {e}")
 
