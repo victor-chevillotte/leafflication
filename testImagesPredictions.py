b664f866e6791f296731189ce967a31b7f1996e5
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-04-18 17:03:28 +0200

    feat: basic working results

--- testImagesPredictions.py
+++ testImagesPredictions.py
@@ -3,14 +3,15 @@ import os
 import argparse
 import re
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 RED = "\033[31m"
 GREEN = "\033[32m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
 
-def testDir(dir_path):
+
+def testDir(dir_path, modelName):
     trueClasses = []
     predictedClasses = []
     files = os.listdir(dir_path)
@@ -19,19 +20,27 @@ def testDir(dir_path):
 
         if os.path.isfile(file_path) and file_path.endswith(".JPG"):
             command = [
-                "python3", "predict.py",
-                "-m", "CamModel.keras",
-                "--i", file_path
+                "python3",
+                "predict.py",
+                "-m",
+                modelName,
+                "--i",
+                file_path,
             ]
 
             result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
             if result.stdout:
-                lines = result.stdout.split('\n')
+                lines = result.stdout.split("\n")
                 for line in lines:
+                    print(line)
                     if "Predicted class :" in line:
-                        predicted_class = line.split(':')[-1].strip()
-                        filename_without_extension = re.sub(r'\.jpe?g$', '', file_name, flags=re.IGNORECASE)
-                        cleaned_filename = re.sub(r'\d+$', '', filename_without_extension)
+                        predicted_class = line.split(":")[-1].strip()
+                        filename_without_extension = re.sub(
+                            r"\.jpe?g$", "", file_name, flags=re.IGNORECASE
+                        )
+                        cleaned_filename = re.sub(
+                            r"\d+$", "", filename_without_extension
+                        )
                         trueClasses.append(cleaned_filename)
                         predictedClasses.append(predicted_class)
                         break
@@ -39,25 +48,31 @@ def testDir(dir_path):
                 print(f"Erreur pour {file_name}:", result.stderr)
     return trueClasses, predictedClasses
 
+
 def main():
     parser = argparse.ArgumentParser(description="Images test prediction")
-    parser.add_argument('-m', type=str, help="Model name")
-    parser.add_argument('--d', type=str, help="Directory path")
+    parser.add_argument("-m", type=str, help="Model name")
+    parser.add_argument("--d", type=str, help="Directory path")
     args = parser.parse_args()
     modelName = f"{args.m}.keras"
     if args.d:
         folder_path = args.d
     else:
-        folder_path = 'data/test_images/Unit_test1'
-    trueClasses, predictedClasses = testDir(folder_path)
+        folder_path = "data/test_images/Unit_test1"
+    trueClasses, predictedClasses = testDir(folder_path, modelName)
     y = 0
     for i in range(len(trueClasses)):
         if trueClasses[i] == predictedClasses[i]:
             y += 1
-            print(f"{GREEN}True class: {trueClasses[i]}, Predicted class: {predictedClasses[i]}{RESET}")
+            print(
+                f"{GREEN}True class: {trueClasses[i]}, Predicted class: {predictedClasses[i]}{RESET}"
+            )
         else:
-            print(f"{RED}True class: {trueClasses[i]}, Predicted class: {predictedClasses[i]}{RESET}")
+            print(
+                f"{RED}True class: {trueClasses[i]}, Predicted class: {predictedClasses[i]}{RESET}"
+            )
     print(f"{RED}Accuracy: {y}/{len(trueClasses)}{RESET}")
+
 
 if __name__ == "__main__":
     main()
