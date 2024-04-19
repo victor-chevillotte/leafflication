def5e58af343e2c8169afd9aa4e14c0c7f37b099
mdesoeuv <88730010+mdesoeuv@users.noreply.github.com>
2024-04-19 14:39:45 +0200

    feat: cli v3 (#18)
    * feat: train CLI
    
    * feat: train CLI
    
    * fix: result color

--- testImagesPredictions.py
+++ testImagesPredictions.py
@@ -3,7 +3,7 @@ import os
 import argparse
 import re
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 RED = "\033[31m"
 GREEN = "\033[32m"
@@ -20,61 +20,68 @@ def testDir(dir_path, model_name):
 
         if os.path.isfile(file_path) and file_path.endswith(".JPG"):
             command = [
-                "python3", "predict.py",
-                "-m", model_name,
-                "--i", file_path
+                "python3",
+                "predict.py",
+                "-m",
+                model_name,
+                "--i",
+                file_path,
             ]
 
             result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
             if result.stdout:
-                lines = result.stdout.split('\n')
+                lines = result.stdout.split("\n")
                 for line in lines:
                     if "Predicted class :" in line:
-                        predicted_class = line.split(':')[-1].strip()
+                        predicted_class = line.split(":")[-1].strip()
                         filename_without_extension = re.sub(
-                            r'\.jpe?g$',
-                            '',
-                            file_name,
-                            flags=re.IGNORECASE
+                            r"\.jpe?g$", "", file_name, flags=re.IGNORECASE
                         )
                         cleaned_filename = re.sub(
-                            r'\d+$',
-                            '',
-                            filename_without_extension
+                            r"\d+$", "", filename_without_extension
                         )
                         trueClasses.append(cleaned_filename)
                         predictedClasses.append(predicted_class)
                         break
             if result.stderr:
-                print(f"Erreur pour {file_name}:", result.stderr)
+                print(f"Error for {file_name}:", result.stderr)
     return trueClasses, predictedClasses
 
 
 def main():
     parser = argparse.ArgumentParser(description="Images test prediction")
-    parser.add_argument('-m', type=str, help="Model name")
-    parser.add_argument('--d', type=str, help="Directory path")
+    parser.add_argument(
+        "-m", "--model", dest="m", type=str, help="Model name", required=True
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        dest="d",
+        required=True,
+        type=str,
+        help="Directory path",
+    )
     args = parser.parse_args()
-    model_name = f"{args.m}.keras"
+    model_name = f"{args.m}.keras" if not args.m.endswith(".keras") else args.m
     if args.d:
         folder_path = args.d
     else:
-        folder_path = 'data/test_images/Unit_test1'
+        folder_path = "data/test_images/Unit_test1"
     trueClasses, predictedClasses = testDir(folder_path, model_name)
     y = 0
     for i in range(len(trueClasses)):
+        COLOR = RED
         if trueClasses[i] == predictedClasses[i]:
             y += 1
-            print(
-                f"{GREEN}True class: {trueClasses[i]}, "
-                f"Predicted class: {predictedClasses[i]}{RESET}"
-            )
-        else:
-            print(
-                f"{RED}True class: {trueClasses[i]}, "
-                f"Predicted class: {predictedClasses[i]}{RESET}"
-            )
-    print(f"{RED}Accuracy: {y}/{len(trueClasses)}{RESET}")
+            COLOR = GREEN
+        print(
+            f"{COLOR}True class: {trueClasses[i]}, "
+            f"Predicted class: {predictedClasses[i]}{RESET}"
+        )
+    COLOR = RED
+    if len(trueClasses) == y:
+        COLOR = GREEN
+    print(f"{COLOR}Accuracy: {y}/{len(trueClasses)}{RESET}")
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -11,35 +11,45 @@ from trainUtils.Utils import Utils
 
 def get_args():
     parser = argparse.ArgumentParser(description="Training")
-    parser.add_argument("-d", type=str, help="Directory path")
-    parser.add_argument("-n", type=str, help="Model name")
-    parser.add_argument("--e", type=int, help="Epochs")
-    parser.add_argument("--b", type=int, help="Batch size")
-    parser.add_argument("--s", type=int, help="Seed")
     parser.add_argument(
-        "--v",
+        "-d", "--directory", type=str, dest="d", help="Directory path"
+    )
+    parser.add_argument("-n", "--name", dest="n", type=str, help="Model name")
+    parser.add_argument("-e", "--epoch", dest="e", type=int, help="Epochs")
+    parser.add_argument("-b", "--batch", dest="b", type=int, help="Batch size")
+    parser.add_argument("-s", "--seed", dest="s", type=int, help="Seed")
+    parser.add_argument(
+        "-v",
+        "--validation",
+        dest="v",
         type=int,
-        help="Percents of validation data"
+        help="Percents of validation data",
     )
     parser.add_argument(
-        "--a",
+        "-a",
+        "--augment",
+        dest="a",
         type=int,
-        help="Augment data with a minimum of images per class"
+        help="Augment data with a minimum of images per class",
     )
     parser.add_argument(
-        "--t",
+        "--no-transform",
         action="store_true",
-        help="Don't transform data"
+        dest="t",
+        help="Don't transform data",
     )
     parser.add_argument(
-        "--r",
+        "--no-augment",
+        dest="r",
         action="store_true",
-        help="Don't augment data"
+        help="Don't augment data",
     )
     parser.add_argument(
-        "--p",
+        "-p",
+        "--patience",
+        dest="p",
         type=int,
-        help="Patience for early stopping"
+        help="Patience for early stopping",
     )
     return parser.parse_args()
 
@@ -60,7 +70,7 @@ def data_augmentation(model_parameters, dir_for_training):
         model_parameters.img_per_class = AugmentData.augment_data(
             dir_for_training,
             model_parameters.augment_options,
-            model_parameters.img_per_class
+            model_parameters.img_per_class,
         )
 
         # Data transformation
@@ -70,8 +80,7 @@ def data_augmentation(model_parameters, dir_for_training):
         ):
             print("----- Transforming data -----")
             TransformData.transform_data(
-                dir_for_training,
-                model_parameters.transform_option
+                dir_for_training, model_parameters.transform_option
             )
 
     Utils.display_histogram_terminal(dir_for_training)
@@ -107,9 +116,7 @@ def get_data(
 
     AUTOTUNE = tf.data.AUTOTUNE
     train_data = (
-        train_data.cache()
-        .shuffle(1000)
-        .prefetch(buffer_size=AUTOTUNE)
+        train_data.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
     )
     validation_data = validation_data.cache().prefetch(buffer_size=AUTOTUNE)
     # for performance
@@ -161,7 +168,7 @@ def main():
             "flipped",
             "rotated",
             "bright",
-            "cropped"
+            "cropped",
         ]
         model_parameters.transform_option = "mask"
         model_parameters.img_size = (256, 256)
@@ -170,34 +177,31 @@ def main():
         data_augmentation(model_parameters, dir_for_training)
 
         # Training
-        (
-            normalized_train_data,
-            normalized_validation_data,
-            class_names
-        ) = get_data(
-            dir_for_training,
-            model_parameters.batch_size,
-            model_parameters.seed,
-            model_parameters.validation_data,
-            model_parameters.img_size[0],
-            model_parameters.img_size[1],
+        (normalized_train_data, normalized_validation_data, class_names) = (
+            get_data(
+                dir_for_training,
+                model_parameters.batch_size,
+                model_parameters.seed,
+                model_parameters.validation_data,
+                model_parameters.img_size[0],
+                model_parameters.img_size[1],
+            )
         )
 
         model = build_model(
             model_parameters.img_size[0],
             model_parameters.img_size[1],
-            len(class_names)
+            len(class_names),
         )
         model.summary()
         stop_early = tf.keras.callbacks.EarlyStopping(
-            monitor='val_loss',
-            patience=model_parameters.patience
+            monitor="val_loss", patience=model_parameters.patience
         )
         model.fit(
             normalized_train_data,
             validation_data=normalized_validation_data,
             epochs=model_parameters.epochs,
-            callbacks=[stop_early]
+            callbacks=[stop_early],
         )
 
         train_score = model.evaluate(normalized_train_data)
@@ -214,11 +218,7 @@ def main():
         if model_parameters.transform_data_flag:
             model_name += "T"
         Utils.save_model(
-            model,
-            model_name,
-            model_parameters,
-            train_score,
-            validation_score
+            model, model_name, model_parameters, train_score, validation_score
         )
     except Exception as e:
         print(f"An error has occured : {e}")
