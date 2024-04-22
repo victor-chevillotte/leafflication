1f86e22b137da737e7a6639a8ddb4a4a3e35f8a2
victor <victor.chevillotte@gmail.com>
2024-04-22 13:43:38 +0200

    Mise à jour des fonctionnalités de recherche et de tri***

---
+++ batch.sh
@@ -0,0 +1,16 @@
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1300;
+
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1000;
+
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 2 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 16 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 16 --augment 1300;
+python3 train.py -d data -n vic -e 2 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 16 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 16 --augment 1300;
+python3 train.py -d data -n vic -e 2 --b 8 --augment 1000;
+python3 train.py -d data -n vic -e 3 --b 8 --augment 1000;
