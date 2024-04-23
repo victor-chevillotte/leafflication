61eed01023ee33e6f60b7ca536a5cc84da31cc90
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-04-23 10:43:39 +0200

    style: flake8

--- getBestModel.py
+++ getBestModel.py
@@ -11,21 +11,30 @@ def main():
             if filename.endswith(".json"):
                 filepath = os.path.join(dir_path, filename)
                 try:
-                    with open(filepath, 'r') as file:
+                    with open(filepath, "r") as file:
                         data = json.load(file)
-                        validation_accuracy = data['score']['validation_accuracy']
-                        if validation_accuracy is not None and validation_accuracy > 0.9:
-                            note = data.get('note')
+                        validation_accuracy = data["score"][
+                            "validation_accuracy"
+                        ]
+                        if (
+                            validation_accuracy is not None
+                            and validation_accuracy > 0.9
+                        ):
+                            note = data.get("note")
                             if note is not None:
-                                note = int(note.split('/')[0])
+                                note = int(note.split("/")[0])
                             else:
                                 note = -1
-                            accuracies.append((filename, validation_accuracy, note))
+                            accuracies.append(
+                                (filename, validation_accuracy, note)
+                            )
                 except Exception as e:
                     print(f"An error has occured : {e}, in {filepath}")
         accuracies.sort(key=lambda x: (x[2], x[1]), reverse=True)
         for model in accuracies:
-            print(f"Model : {model[0]} - Accuracy : {model[1]}, Note : {model[2]}")
+            print(
+                f"Model: {model[0]} - Accuracy: {model[1]}, Note: {model[2]}"
+            )
 
     except Exception as e:
         print(f"An error has occured : {e}, in ")
